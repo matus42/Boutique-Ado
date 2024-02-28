9454ba5335640a8bec05dba9453d3a5edae1ce9b
matus42 <134312272+matus42@users.noreply.github.com>
2024-02-28 16:16:20 +0000

    Added homepage content

--- home/views.py
+++ home/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
     """A view that displays the index page"""
-    
+
     return render(request, 'home/index.html')
